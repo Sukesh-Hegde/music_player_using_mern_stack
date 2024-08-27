5cabd2e5157c2cb032620f48ba4d28c49c795390
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-27 15:15:55 +0530

    completed navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,54 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+    // const [keyword, setKeyword] = useState("");
+    // const [message, setMessage] = useState("");
+    const [tracks, setTracks] = useState([]);
+    // const [token, setToken] = useState(null);
+
+    const fetchMusicData = async () => {
+      setTracks([]);
+      window.scrollTo(0, 0);
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.spotify.com/v1/search?q=${keyword}&type=track&offset=${resultOffset}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch music data");
+        }
+
+        const jsonData = await response.json();
+
+        setTracks(jsonData.tracks.items);
+      } catch (error) {
+        setMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+
   return (
-    <div className="App">
-      <h1>welcome</h1>
-    </div>
+    <>
+      <Navbar/>
+
+      <div className="container">
+        <div className="row">
+
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,50 @@
+// import React, { useContext } from "react";
+
+// import { Link } from "react-router-dom";
+// import { MusicContext } from "../Context";
+// import PinnedMusic from "./PinnedMusic";
+// import LikedMusic from "./LikedMusic";
+
+const Navbar = () => {
+  // const musicContext = useContext(MusicContext);
+  // const likedMusic = musicContext.likedMusic;
+  // const pinnedMusic = musicContext.pinnedMusic;
+  // const setResultOffset = musicContext.setResultOffset;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      Music-App
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+ 
+        <input
+          className="form-control me-2 w-75"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+    </div>
+  </div>
+</nav>
+</>
+
+  );
+};
+
+export default Navbar;
