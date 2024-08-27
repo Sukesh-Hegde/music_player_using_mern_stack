97d60c8300534c1662cdab8ea11f7da7f77c4b52
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-27 22:23:50 +0530

    implemented singup login

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const app = express();
 const cors = require("cors");
 
 const connection = require("./db");
-// const userRoutes = require("./routes/users");
-// const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
 // database connection
 connection();
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-// app.use("/api/users", userRoutes);
-// app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
